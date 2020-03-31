6cc57dd57624383d8fc79a06cfe4c88e140d7b8f
Jan Sepke <625043+jansepke@users.noreply.github.com>
2020-03-31 21:48:14 +0200

    first website

---
+++ generate-data.js
@@ -0,0 +1,31 @@
+const csv = require("csvtojson");
+const fs = require("fs").promises;
+
+// World http://www.msci.com/eqb/esg/performance/106.0.all.xls
+// ACWI http://www.msci.com/eqb/esg/performance/2591.0.all.xls
+// ACWI IMI http://www.msci.com/eqb/esg/performance/73562.0.all.xls
+
+const parseFile = async fileName =>
+  csv({
+    colParser: {
+      Price: item => parseFloat(item.replace(",", "")),
+      Date: item => new Date(item)
+    }
+  }).fromFile(fileName);
+
+const processIndex = async index => {
+  const msciData = await parseFile(`msci/${index}.csv`);
+  const investingData = (await parseFile(`investing/${index}.csv`)).reverse();
+
+  const result = msciData
+    .concat(investingData)
+    .map(({ Date: date, Price: price }) => ({ date, price: price.toFixed(2) }));
+
+  await fs.writeFile(`./docs/data/${index}.json`, JSON.stringify(result));
+};
+
+(async () => {
+  for (const index of ["world", "acwi", "acwi-imi"]) {
+    await processIndex(index);
+  }
+})();
