93550851b380181a4ad684556adca098c05da0ca
Jan Sepke <625043+jansepke@users.noreply.github.com>
2020-04-01 10:11:17 +0200

    add minDrawdown slider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { Container } from "@material-ui/core";
+import { Container, Slider, Typography } from "@material-ui/core";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -16,59 +16,80 @@ const dtf = new Intl.DateTimeFormat("en", {
   day: "2-digit"
 });
 
-const minPercent = 20;
 const msToDays = 1 / 60 / 60 / 24 / 1000;
 
-function App() {
+const calculateTableData = (data, minDrawdown) => {
+  const newTableData = [];
+  let lastPeak = { price: 0 },
+    lastTrough = { price: 0 };
+
+  data = data.map(({ date, price }) => ({
+    date: new Date(date),
+    price: parseFloat(price)
+  }));
+
+  const checkDrawdown = (newPeak = {}) => {
+    const percent = (1 - lastTrough.price / lastPeak.price) * 100;
+    if (percent > minDrawdown) {
+      const daysDown = (lastTrough.date - lastPeak.date) * msToDays;
+      const daysDone = (newPeak.date - lastPeak.date) * msToDays;
+
+      newTableData.push({
+        startDate: dtf.format(lastPeak.date),
+        endDate: dtf.format(lastTrough.date),
+        daysDown: daysDown.toFixed(),
+        percent: -percent.toFixed(),
+        daysDone: daysDone.toFixed()
+      });
+    }
+  };
+
+  for (const entry of data) {
+    if (entry.price > lastPeak.price) {
+      checkDrawdown(entry);
+      lastPeak = entry;
+      lastTrough = entry;
+    } else if (entry.price < lastTrough.price) {
+      lastTrough = entry;
+    }
+  }
+
+  checkDrawdown();
+
+  return newTableData;
+};
+
+const App = () => {
   const [tableData, setTableData] = useState([]);
+  const [minDrawdown, setMinDrawdown] = useState(30);
+
+  const handleSliderChange = (event, newValue) => {
+    setMinDrawdown(newValue);
+  };
 
   useEffect(() => {
     (async () => {
       let data = await import("../data/world.json");
 
-      const newTableData = [];
-      let lastPeak = { price: 0 },
-        lastTrough = { price: 0 };
-
-      data = data.map(({ date, price }) => ({
-        date: new Date(date),
-        price: parseFloat(price)
-      }));
-
-      const checkDrawdown = (newPeak = {}) => {
-        const percent = (1 - lastTrough.price / lastPeak.price) * 100;
-        if (percent > minPercent) {
-          const daysDown = (lastTrough.date - lastPeak.date) * msToDays;
-          const daysDone = (newPeak.date - lastPeak.date) * msToDays;
-
-          newTableData.push({
-            startDate: dtf.format(lastPeak.date),
-            endDate: dtf.format(lastTrough.date),
-            daysDown: daysDown.toFixed(),
-            percent: -percent.toFixed(),
-            daysDone: daysDone.toFixed()
-          });
-        }
-      };
-
-      for (const entry of data) {
-        if (entry.price > lastPeak.price) {
-          checkDrawdown(entry);
-          lastPeak = entry;
-          lastTrough = entry;
-        } else if (entry.price < lastTrough.price) {
-          lastTrough = entry;
-        }
-      }
-
-      checkDrawdown();
+      const newTableData = calculateTableData(data, minDrawdown);
 
       setTableData(newTableData);
     })();
-  }, []);
+  }, [minDrawdown]);
 
   return (
     <Container maxWidth="md">
+      <Typography gutterBottom>Minimum drawdown</Typography>
+      <Slider
+        value={minDrawdown}
+        onChange={handleSliderChange}
+        valueLabelDisplay="auto"
+        step={5}
+        marks
+        valueLabelDisplay="on"
+        min={10}
+        max={50}
+      />
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -95,6 +116,6 @@ function App() {
       </TableContainer>
     </Container>
   );
-}
+};
 
 ReactDOM.render(<App />, document.querySelector("#app"));
